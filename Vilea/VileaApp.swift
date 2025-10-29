1cae4af88794277a9b7281db00dc46aa629a46dd
Hans Rietmann <hans.rietmann@icloud.com>
2025-10-29 15:08:27 +0100

    Initial Commit

---
+++ Vilea/VileaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VileaApp.swift
+//  Vilea
+//
+//  Created by Hans Rietmann on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct VileaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
